9f9c397be998cd2ca7063adda911aa08b7918e74
unknown <marcosh.craft@gmail.com>
2023-11-28 09:03:22 +0100

    started developing macros calculator inputs

--- src/app/macros-calculator/page.tsx
+++ src/app/macros-calculator/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 function Page() {
   return (
@@ -7,6 +9,30 @@ function Page() {
       <div>
         <Button>Metric</Button>
         <Button>Imperial</Button>
+      </div>
+
+      <div>
+        <Label>Weight</Label>
+        <Input />
+      </div>
+
+      <div>
+        <Label>Do you know your body fat %?</Label>
+      </div>
+
+      <div>
+        <Label>Body fat %</Label>
+        <Input />
+      </div>
+
+      <div>
+        <Label>Height</Label>
+        <Input />
+      </div>
+
+      <div>
+        <Label>Age</Label>
+        <Input />
       </div>
     </div>
   );
