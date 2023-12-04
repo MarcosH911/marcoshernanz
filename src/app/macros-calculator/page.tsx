d73fe5e4681e80ce8bd9903adcbec53afa44eaf0
unknown <marcosh.craft@gmail.com>
2023-12-04 09:10:23 +0100

    continued developing macro calculator

--- src/app/macros-calculator/page.tsx
+++ src/app/macros-calculator/page.tsx
@@ -1,40 +1,70 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { useState } from "react";
 
 function Page() {
+  const [measureSystem, setMeasureSystem] = useState<"metric" | "imperial">(
+    "metric"
+  );
+  const [gender, setGender] = useState<null | "male" | "female">(null);
+  const [weight, setWeight] = useState("");
+  const [height, setHeight] = useState("");
+  const [age, setAge] = useState("");
+  const [knowsBodyFat, setKnowsBodyFat] = useState(false);
+  const [bodyFat, setBodyFat] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h1>Macros Calculator</h1>
       <div>
-        <Button>Metric</Button>
-        <Button>Imperial</Button>
+        <Button onClick={() => setMeasureSystem("metric")}>Metric</Button>
+        <Button onClick={() => setMeasureSystem("imperial")}>Imperial</Button>
+      </div>
+
+      <div>
+        <Button onClick={() => setGender("male")}>Male</Button>
+        <Button onClick={() => setGender("female")}>Female</Button>
       </div>
 
       <div>
         <Label>Weight</Label>
-        <Input />
+        <Input value={weight} onChange={(e) => setWeight(e.target.value)} />
       </div>
 
       <div>
-        <Label>Do you know your body fat %?</Label>
+        <Label htmlFor="knowsBodyFat">Do you know your body fat %?</Label>
+        <Switch
+          id="knowsBodyFat"
+          checked={knowsBodyFat}
+          onCheckedChange={setKnowsBodyFat}
+        />
       </div>
 
-      <div>
-        <Label>Body fat %</Label>
-        <Input />
-      </div>
+      {knowsBodyFat && (
+        <div>
+          <Label>Body fat %</Label>
+          <Input value={bodyFat} onChange={(e) => setBodyFat(e.target.value)} />
+        </div>
+      )}
 
       <div>
         <Label>Height</Label>
-        <Input />
+        <Input value={height} onChange={(e) => setHeight(e.target.value)} />
       </div>
 
       <div>
         <Label>Age</Label>
-        <Input />
+        <Input value={age} onChange={(e) => setAge(e.target.value)} />
       </div>
-    </div>
+    </form>
   );
 }
 
