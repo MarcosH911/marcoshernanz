ba8cc579c686270a2f2b1f7a23136ca78725899b
unknown <marcosh.craft@gmail.com>
2023-12-13 08:59:05 +0100

    made conversion from imperial to metric to calculate bmr

--- src/app/macros-calculator/page.tsx
+++ src/app/macros-calculator/page.tsx
@@ -7,9 +7,9 @@ import { Switch } from "@/components/ui/switch";
 import { useState } from "react";
 
 function Page() {
-  const [measureSystem, setMeasureSystem] = useState<"metric" | "imperial">(
-    "metric"
-  );
+  const [measurementSystem, setMeasurementSystem] = useState<
+    "metric" | "imperial"
+  >("metric");
   const [gender, setGender] = useState<null | "male" | "female">(null);
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
@@ -20,14 +20,24 @@ function Page() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    let metricWeight = Number(weight);
+    let metricHeight = Number(height);
+
+    if (measurementSystem === "imperial") {
+      metricWeight = Number(weight) * 0.453592;
+      metricHeight =
+        Number(height.split("-")[0]) * 30.48 +
+        Number(height.split("-")[1]) * 2.54;
+    }
+
     let bmr;
 
     if (knowsBodyFat && Number(bodyFat) <= 25) {
-      bmr = 370 + 21.6 * (1 - Number(bodyFat)) * Number(weight);
+      bmr = 370 + 21.6 * (1 - Number(bodyFat) / 100) * metricWeight;
     } else if (gender === "male") {
-      bmr = 10 * Number(weight) + 6.25 * Number(height) - 5 * Number(age) + 5;
+      bmr = 10 * metricWeight + 6.25 * metricHeight - 5 * Number(age) + 5;
     } else {
-      bmr = 10 * Number(weight) + 6.25 * Number(height) - 5 * Number(age) - 161;
+      bmr = 10 * metricWeight + 6.25 * metricHeight - 5 * Number(age) - 161;
     }
   };
 
@@ -36,8 +46,10 @@ function Page() {
       <h1>Macros Calculator</h1>
       <div>
         <Label>System</Label>
-        <Button onClick={() => setMeasureSystem("metric")}>Metric</Button>
-        <Button onClick={() => setMeasureSystem("imperial")}>Imperial</Button>
+        <Button onClick={() => setMeasurementSystem("metric")}>Metric</Button>
+        <Button onClick={() => setMeasurementSystem("imperial")}>
+          Imperial
+        </Button>
       </div>
 
       <div>
